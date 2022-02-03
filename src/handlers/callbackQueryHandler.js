ba9a1eb35be20b07e73e32233bdc2d303cf06ae2
moskalevNick1 <n.moskalev@freshlimesoft.com>
2022-02-03 15:15:35 +0300

    send hw

--- src/handlers/callbackQueryHandler.js
+++ src/handlers/callbackQueryHandler.js
@@ -3,168 +3,261 @@ const {
   packsNative,
   packsRussian,
   teachers,
+  teacherStudentOptions,
+  gradeLesson,
+  formOfHomeWork,
 } = require('../options/index');
 const bot = require('../services/bot');
-const {getTeacherById} = require('../services/user');
+const { getTeacherById } = require('../services/user');
 
 const chatIdNikolay = 108758719;
 
 const callbackQueryHandler = async (msg) => {
+  const data = msg.data;
+  const chatId = msg.message.chat.id;
 
-    const data = msg.data;
-    const chatId = msg.message.chat.id;
-  
-    const isStudentForTeacher = data.split('-')[0] === 'studentForTeacher';
-  
-    if (isStudentForTeacher) {
-      // bot.removeAllListeners('callback_query');
-  
-      const currentStudentChatId = data.split('-')[1];
+  const isStudentForTeacher = data.split('-')[0] === 'studentForTeacher';
+  const isRequestEndLesson = data.split('-')[0] === 'requestEndLesson';
+  const isSendHomeWork = data.split('-')[0] === 'sendHomeWork';
+  const isPictureHW = data.split('-')[0] === 'pictureHW';
+  const isDocumentHW = data.split('-')[0] === 'documentHW';
+  const isTextHW = data.split('-')[0] === 'textHW';
 
-      console.log('currentStudentChatId', currentStudentChatId);
-  
-      // bot.on('callback_query', async (msg) => {
-      //   const data = msg.data;
-      //   const chatId = msg.message.chat.id;
-      //   console.log(data);
-      // });
-  
-      // bot.sendMessage(currentStudentChatId, `выберите действие:`, studentLeasonOptions(currentStudentChatId));
-    } else {
-      switch (data) {
-        case 'teacherName': {
-          return bot.sendMessage(
-            chatId,
-            `вашего преподавателя зовут Пупа Луповна`
-          );
-        }
+  /* if (isPictureHW) {
+    const currentStudentChatId = data.split('-')[1];
+    await bot.sendMessage(
+      chatId,
+      'следующий документ будет отправлен вашему студенту',
+      gradeLesson
+    );
+  } */
 
-        case 'lessonsLeft': {
-          return bot.sendMessage(chatId, `у вас осталось 7 уроков`);
-        }
-  
-        case 'moralSupport': {
-          return bot.sendMessage(chatId, `все буде добра!`);
-        }
-  
-        case 'communicationRequest': {
-          return bot.sendMessage(
-            chatIdNikolay,
-            `Пользователь ${msg.from.first_name} ${
-              msg.from.last_name ? msg.from.last_name : ''
-            } (никнейм: ${msg.from.username}) просит помощи`
-          );
-        }
-  
-        case 'messageForTeacher': {
-          bot.sendMessage(
-            chatId,
-            `следующее сообщение которое вы мне отправите будет автоматически переадресовано вашему преподавателю`
-          );
-  
-          bot.on('message', (msg) => {
-            if (msg.text[0] !== '/') {
-              bot.sendMessage(
-                chatIdNikolay,
-                `Пользователь ${msg.from.first_name} ${
-                  msg.from.last_name ? msg.from.last_name : ''
-                } написал вам: \n ${msg.text}`
-              );
-              bot.sendMessage(
-                chatId,
-                'ваше сообщение успешно отправлено, скоро я вам пришлю ответ',
-                teacherOptions
-              );
-              bot.removeAllListeners('message');
-              start();
-            }
-          });
-          return;
-        }
-  
-        case 'back': {
-          return bot.sendMessage(
-            chatId,
-            ` ${msg.from.first_name} ${
-              msg.from.last_name ? msg.from.last_name : ''
-            } выберите из опций что хотите узнать`
-          );
-        }
-  
-        case 'listOfStudents': {
+  if (isRequestEndLesson) {
+    const currentStudentChatId = data.split('-')[1];
 
-          const teacher = await getTeacherById(msg.from.id);
+    await bot.sendMessage(
+      chatId,
+      'Ваша заявка сформирована, оцените урок',
+      gradeLesson
+    );
 
-          const arrayStudents = teacher.students.map((student) => ([{
+    await bot.sendMessage(
+      currentStudentChatId,
+      'Если урок проведен, поставьте оценку вашему преподавателю: ',
+      gradeLesson
+    );
+
+    return bot.sendMessage(chatId, 'спасибо за работу');
+  }
+
+  if (isSendHomeWork) {
+    const currentStudentChatId = data.split('-')[1];
+    bot.sendMessage(chatId, 'следующее сообщение будет доставлено ученику:');
+
+    bot.on('message', (msg) => {
+      bot.sendMessage(chatIdNikolay, `hw ${msg.document}`);
+      console.log('document', msg);
+      bot.removeAllListeners('message');
+      start();
+    });
+
+    /*  bot.on('photo', (msg) => {
+      console.log('photo', msg);
+    });
+
+    bot.on('text', (msg) => {
+      console.log('text', msg);
+    });
+
+    bot.on('voice', (msg) => {
+      console.log('voice', msg);
+    });
+
+    bot.on('message', (msg) => {
+      if (msg.text[0] !== '/') {
+        console.log('hw: ', msg);
+        // bot.sendMessage(chatIdNikolay, 'ваше домашнее задание: ', )
+        bot.removeAllListeners('message');
+        start();
+      }
+    }); */
+    return;
+  }
+
+  if (isStudentForTeacher) {
+    const currentStudentChatId = data.split('-')[1];
+    return bot.sendMessage(
+      chatId,
+      `выберите действие:`,
+      teacherStudentOptions(currentStudentChatId)
+    );
+  } else {
+    switch (data) {
+      case 'teacherName': {
+        return bot.sendMessage(
+          chatId,
+          `вашего преподавателя зовут Пупа Луповна`
+        );
+      }
+
+      case 'lessonsLeft': {
+        return bot.sendMessage(chatId, `у вас осталось 7 уроков`);
+      }
+
+      case 'moralSupport': {
+        return bot.sendMessage(chatId, `все буде добра!`);
+      }
+
+      case 'communicationRequest': {
+        return bot.sendMessage(
+          chatIdNikolay,
+          `Пользователь ${msg.from.first_name} ${
+            msg.from.last_name ? msg.from.last_name : ''
+          } (никнейм: ${msg.from.username}) просит помощи`
+        );
+      }
+
+      case 'messageForTeacher': {
+        bot.sendMessage(
+          chatId,
+          `следующее сообщение которое вы мне отправите будет автоматически переадресовано вашему преподавателю`
+        );
+
+        bot.on('message', (msg) => {
+          if (msg.text[0] !== '/') {
+            bot.sendMessage(
+              chatIdNikolay,
+              `Пользователь ${msg.from.first_name} ${
+                msg.from.last_name ? msg.from.last_name : ''
+              } написал вам: \n ${msg.text}`
+            );
+            bot.sendMessage(
+              chatId,
+              'ваше сообщение успешно отправлено, скоро я вам пришлю ответ',
+              teacherOptions
+            );
+            bot.removeAllListeners('message');
+            start();
+          }
+        });
+        return;
+      }
+
+      case 'gradeLesson-1': {
+        return bot.sendMessage(
+          chatIdNikolay,
+          `${chatId}, урок окончен, оценка урока 1`
+        );
+      }
+      case 'gradeLesson-2': {
+        return bot.sendMessage(
+          chatIdNikolay,
+          `${chatId}, урок окончен, оценка урока 2`
+        );
+      }
+      case 'gradeLesson-3': {
+        return bot.sendMessage(
+          chatIdNikolay,
+          `${chatId}, урок окончен, оценка урока 3`
+        );
+      }
+      case 'gradeLesson-4': {
+        return bot.sendMessage(
+          chatIdNikolay,
+          `${chatId}, урок окончен, оценка урока 4`
+        );
+      }
+      case 'gradeLesson-5': {
+        return bot.sendMessage(
+          chatIdNikolay,
+          `${chatId}, урок окончен, оценка урока 5`
+        );
+      }
+
+      case 'back': {
+        return bot.sendMessage(
+          chatId,
+          ` ${msg.from.first_name} ${
+            msg.from.last_name ? msg.from.last_name : ''
+          } выберите из опций что хотите узнать`
+        );
+      }
+
+      case 'listOfStudents': {
+        const teacher = await getTeacherById(msg.from.id);
+
+        const arrayStudents = teacher.students.map((student) => [
+          {
             text: `${student.name}`,
-            callback_data: `studentForTeacher-${student.chatId}`
-          }]));
-  
-          return bot.sendMessage(chatId, 'Выберите ученика: ', {
-            reply_markup: JSON.stringify({
-              inline_keyboard: arrayStudents,
-            }),
-          });
-        }
-  
-        case 'teachers': {
-          return bot.sendMessage(chatId, 'Выберите преподавателя:', teachers);
-        }
-  
-        case 'nativeSpeaker': {
-          return bot.sendMessage(chatId, `Выберите пакет:`, packsNative);
-        }
+            callback_data: `studentForTeacher-${student.chatId}`,
+          },
+        ]);
 
-        case 'russianSpeaker': {
-          return bot.sendMessage(chatId, `Выберите пакет:`, packsRussian);
-        }
-  
-        case 'individualRussian': {
-          return bot.sendMessage(
-            chatId,
-            `Индивидуальная форма \nРусскоязычный преподаватель: \nПакет 4: \n50 мин – 109.99 руб. цена за урок 27.50 \n80 мин – 169.99 руб. цена за урок 42.50 \nПакет 8: \n50 мин – 214.99 руб. цена за урок 26.90 \n80 мин –325.99 руб. цена за урок 40.75 \nПакет 16:\n50 мин –399.99 руб. цена за урок 24.99\n80 мин –595.99 руб. цена за урок 37.25\nПакет 24:\n50 мин –559.99 руб. цена за урок 23.30\n80 мин –839.99 руб. цена за урок 34.99`,
-            packsRussian
-          );
-        }
+        return bot.sendMessage(chatId, 'Выберите ученика: ', {
+          reply_markup: JSON.stringify({
+            inline_keyboard: arrayStudents,
+          }),
+        });
+      }
 
-        case 'pairsRussian': {
-          return bot.sendMessage(
-            chatId,
-            `Парная форма\nРусскоязычный преподаватель:\nПакет 4:\n50 мин – 109.99 руб. цена за урок 27.50\n80 мин – 169.99 руб. цена за урок 42.50\nПакет 8:\n50 мин – 214.99 руб. цена за урок 26.90\n80 мин –325.99 руб. цена за урок 40.75\nПакет 16:\n50 мин –399.99 руб. цена за урок 24.998\n80 мин –595.99 руб. цена за урок 37.25\nПакет 24:\n50 мин –559.99 руб. цена за урок 23.30\n80 мин –839.99 руб. цена за урок 34.99`,
-            packsRussian
-          );
-        }
+      case 'teachers': {
+        return bot.sendMessage(chatId, 'Выберите преподавателя:', teachers);
+      }
 
-        case 'groupsRussian': {
-          return bot.sendMessage(
-            chatId,
-            `Групповая форма\nРусскоязычный преподаватель:\nПакет 4:\n50 мин – 109.99 руб. цена за урок 27.50\n80 мин – 169.99 руб. цена за урок 42.50\nПакет 8:\n50 мин – 214.99 руб. цена за урок 26.90\n80 мин –325.99 руб. цена за урок 40.75\nПакет 16:\n50 мин –399.99 руб. цена за урок 24.99\n80 мин –595.99 руб. цена за урок 37.25\nПакет 24:\n50 мин –559.99 руб. цена за урок 23.30\n80 мин –839.99 руб. цена за урок 34.99`,
-            packsRussian
-          );
-        }
+      case 'nativeSpeaker': {
+        return bot.sendMessage(chatId, `Выберите пакет:`, packsNative);
+      }
 
-        case 'individualNative': {
-          return bot.sendMessage(
-            chatId,
-            `Индивидуальная форма\nНоситель языка:\nПакет 4:\n50 мин – 169.99 руб. цена за урок 42.50\n80 мин –269.99 руб. цена за урок 67.50\nПакет 8:\n50 мин – 325.99 руб. цена за урок 40.75\n80 мин –519.99 руб. цена за урок 64.99\nПакет 16:\n50 мин – 595.99 руб. цена за урок 37.25\n80 мин –955.99 руб. цена за урок 59.75\nПакет 24:\n50 мин –839.99 руб. цена за урок 34.99\n80 мин –1345.99 руб. цена за урок 56.10`,
-            packsNative
-          );
-        }
+      case 'russianSpeaker': {
+        return bot.sendMessage(chatId, `Выберите пакет:`, packsRussian);
+      }
 
-        case 'pairsNative': {
-          return bot.sendMessage(
-            chatId,
-            `Парная форма\nНоситель языка:\nПакет 4:\n60 мин –109.99 руб. цена за урок 27.50\n80 мин –149.99 руб. цена за урок 36.50\nПакет 8:\n60 мин –214.99 руб. цена за урок 26.90\n80 мин –285.99 руб. цена за урок 35.75\nПакет 16:\n60 мин –405.99 руб. цена за урок 25.40\n80 мин –539.99 руб. цена за урок 33.75\nПакет 24:\n60 мин –559.99 руб. цена за урок 23.30\n120 мин –745.99 руб. цена за урок 31.10`,
-            packsNative
-          );
-        }
+      case 'individualRussian': {
+        return bot.sendMessage(
+          chatId,
+          `Индивидуальная форма \nРусскоязычный преподаватель: \nПакет 4: \n50 мин – 109.99 руб. цена за урок 27.50 \n80 мин – 169.99 руб. цена за урок 42.50 \nПакет 8: \n50 мин – 214.99 руб. цена за урок 26.90 \n80 мин –325.99 руб. цена за урок 40.75 \nПакет 16:\n50 мин –399.99 руб. цена за урок 24.99\n80 мин –595.99 руб. цена за урок 37.25\nПакет 24:\n50 мин –559.99 руб. цена за урок 23.30\n80 мин –839.99 руб. цена за урок 34.99`,
+          packsRussian
+        );
+      }
 
-        default: {
-          // console.log(data);
-          return;
-        }
+      case 'pairsRussian': {
+        return bot.sendMessage(
+          chatId,
+          `Парная форма\nРусскоязычный преподаватель:\nПакет 4:\n50 мин – 109.99 руб. цена за урок 27.50\n80 мин – 169.99 руб. цена за урок 42.50\nПакет 8:\n50 мин – 214.99 руб. цена за урок 26.90\n80 мин –325.99 руб. цена за урок 40.75\nПакет 16:\n50 мин –399.99 руб. цена за урок 24.998\n80 мин –595.99 руб. цена за урок 37.25\nПакет 24:\n50 мин –559.99 руб. цена за урок 23.30\n80 мин –839.99 руб. цена за урок 34.99`,
+          packsRussian
+        );
+      }
+
+      case 'groupsRussian': {
+        return bot.sendMessage(
+          chatId,
+          `Групповая форма\nРусскоязычный преподаватель:\nПакет 4:\n50 мин – 109.99 руб. цена за урок 27.50\n80 мин – 169.99 руб. цена за урок 42.50\nПакет 8:\n50 мин – 214.99 руб. цена за урок 26.90\n80 мин –325.99 руб. цена за урок 40.75\nПакет 16:\n50 мин –399.99 руб. цена за урок 24.99\n80 мин –595.99 руб. цена за урок 37.25\nПакет 24:\n50 мин –559.99 руб. цена за урок 23.30\n80 мин –839.99 руб. цена за урок 34.99`,
+          packsRussian
+        );
+      }
+
+      case 'individualNative': {
+        return bot.sendMessage(
+          chatId,
+          `Индивидуальная форма\nНоситель языка:\nПакет 4:\n50 мин – 169.99 руб. цена за урок 42.50\n80 мин –269.99 руб. цена за урок 67.50\nПакет 8:\n50 мин – 325.99 руб. цена за урок 40.75\n80 мин –519.99 руб. цена за урок 64.99\nПакет 16:\n50 мин – 595.99 руб. цена за урок 37.25\n80 мин –955.99 руб. цена за урок 59.75\nПакет 24:\n50 мин –839.99 руб. цена за урок 34.99\n80 мин –1345.99 руб. цена за урок 56.10`,
+          packsNative
+        );
+      }
+
+      case 'pairsNative': {
+        return bot.sendMessage(
+          chatId,
+          `Парная форма\nНоситель языка:\nПакет 4:\n60 мин –109.99 руб. цена за урок 27.50\n80 мин –149.99 руб. цена за урок 36.50\nПакет 8:\n60 мин –214.99 руб. цена за урок 26.90\n80 мин –285.99 руб. цена за урок 35.75\nПакет 16:\n60 мин –405.99 руб. цена за урок 25.40\n80 мин –539.99 руб. цена за урок 33.75\nПакет 24:\n60 мин –559.99 руб. цена за урок 23.30\n120 мин –745.99 руб. цена за урок 31.10`,
+          packsNative
+        );
+      }
+
+      default: {
+        // console.log(data);
+        return;
       }
     }
   }
+};
 
-  module.exports = callbackQueryHandler;
+module.exports = callbackQueryHandler;
--- src/options/index.js
+++ src/options/index.js
@@ -1,157 +1,260 @@
 const {
-    back,
-    lessonsLeft,
-    teacherName,
-    teachers,
-    messageForTeacher,
-    listOfStudents,
-    russianSpeaker,
-    nativeSpeaker,
-    individualRussian,
-    pairsRussian,
-    groupsRussian,
-    moralSupport,
-    communicationRequest,
-    individualNative,
-    pairsNative
+  back,
+  lessonsLeft,
+  teacherName,
+  teachers,
+  messageForTeacher,
+  listOfStudents,
+  russianSpeaker,
+  nativeSpeaker,
+  individualRussian,
+  pairsRussian,
+  groupsRussian,
+  moralSupport,
+  communicationRequest,
+  individualNative,
+  pairsNative,
 } = require('../text/callbackDataText');
 
 const studentOptions = {
-    reply_markup: JSON.stringify({
-      inline_keyboard: [
-        [{
-            text: 'Как обращаться к моему преподавателю',
-            callback_data: teacherName,
-        }],
-        [{ 
-            text: 'Сколько занятий осталось', 
-            callback_data: lessonsLeft 
-        }],
-        [{
-            text: 'Написать сообщение преподавателю',
-            callback_data: messageForTeacher,
-        }],
-        [{ 
-            text: 'Вернуться', 
-            callback_data: back 
-        }],
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [
+        {
+          text: 'Как обращаться к моему преподавателю',
+          callback_data: teacherName,
+        },
       ],
-    }),
+      [
+        {
+          text: 'Сколько занятий осталось',
+          callback_data: lessonsLeft,
+        },
+      ],
+      [
+        {
+          text: 'Написать сообщение преподавателю',
+          callback_data: messageForTeacher,
+        },
+      ],
+      [
+        {
+          text: 'Вернуться',
+          callback_data: back,
+        },
+      ],
+    ],
+  }),
 };
 
 const teacherOptions = {
-    reply_markup: JSON.stringify({
-      inline_keyboard: [
-        [{ 
-            text: 'Список моих студентов', 
-            callback_data: listOfStudents 
-        }],
-        [{ 
-            text: 'Вернуться', 
-            callback_data: back 
-        }],
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [
+        {
+          text: 'Список моих студентов',
+          callback_data: listOfStudents,
+        },
       ],
-    }),
+      [
+        {
+          text: 'Вернуться',
+          callback_data: back,
+        },
+      ],
+    ],
+  }),
 };
 
 const teachersOptions = {
-    reply_markup: JSON.stringify({
-      inline_keyboard: [
-        [{
-            text: 'Русскоговорящий преподаватель',
-            callback_data: russianSpeaker,
-        }],
-        [{
-            text: 'Носитель языка',
-            callback_data: nativeSpeaker,
-        }],
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [
+        {
+          text: 'Русскоговорящий преподаватель',
+          callback_data: russianSpeaker,
+        },
       ],
-    }),
+      [
+        {
+          text: 'Носитель языка',
+          callback_data: nativeSpeaker,
+        },
+      ],
+    ],
+  }),
 };
 
 const packsRussian = {
-    reply_markup: JSON.stringify({
-      inline_keyboard: [
-        [{
-            text: 'Индивидуальная',
-            callback_data: individualRussian,
-        }],
-        [{ 
-            text: 'Парная', 
-            callback_data: pairsRussian 
-        }],
-        [{ 
-            text: 'Групповая', 
-            callback_data: groupsRussian 
-        }],
-        [{ 
-            text: 'Сменить преподавателя', 
-            callback_data: teachers 
-        }],
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [
+        {
+          text: 'Индивидуальная',
+          callback_data: individualRussian,
+        },
       ],
-    }),
+      [
+        {
+          text: 'Парная',
+          callback_data: pairsRussian,
+        },
+      ],
+      [
+        {
+          text: 'Групповая',
+          callback_data: groupsRussian,
+        },
+      ],
+      [
+        {
+          text: 'Сменить преподавателя',
+          callback_data: teachers,
+        },
+      ],
+    ],
+  }),
 };
 
 const packsNative = {
-    reply_markup: JSON.stringify({
-      inline_keyboard: [
-        [{
-            text: 'Индивидуальная',
-            callback_data: individualNative,
-        }],
-        [{ 
-            text: 'Парная', 
-            callback_data: pairsNative 
-        }],
-        [{ 
-            text: 'Сменить преподавателя', 
-            callback_data: teachers 
-        }],
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [
+        {
+          text: 'Индивидуальная',
+          callback_data: individualNative,
+        },
       ],
-    }),
+      [
+        {
+          text: 'Парная',
+          callback_data: pairsNative,
+        },
+      ],
+      [
+        {
+          text: 'Сменить преподавателя',
+          callback_data: teachers,
+        },
+      ],
+    ],
+  }),
 };
 
 const supportOptions = {
-    reply_markup: JSON.stringify({
-      inline_keyboard: [
-        [{ 
-            text: 'Поддержите морально', 
-            callback_data: moralSupport 
-        }],
-        [{
-            text: 'Свяжитесь со мной',
-            callback_data: communicationRequest,
-        }],
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [
+        {
+          text: 'Поддержите морально',
+          callback_data: moralSupport,
+        },
       ],
-    }),
+      [
+        {
+          text: 'Свяжитесь со мной',
+          callback_data: communicationRequest,
+        },
+      ],
+    ],
+  }),
 };
 
-const studentLeasonOptions = (chatId) => ({
-    reply_markup: JSON.stringify({
-      inline_keyboard: [
-        [{
-            text: 'Урок проведен',
-            callback_data: `requestEndLesson-${chatId}`,
-        }],
-        [{
-            text: 'Выслать домашнее задание',
-            callback_data: `sendHomeWork-${chatId}`,
-        }],
-        [{
-            text: 'Запрос переноса урока',
-            callback_data: `requestMoveLesson-${chatId}`,
-        }],
+const teacherStudentOptions = (chatId) => ({
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [
+        {
+          text: 'Урок проведен',
+          callback_data: `requestEndLesson-${chatId}`,
+        },
       ],
-    }),
-})
+      [
+        {
+          text: 'Выслать домашнее задание',
+          callback_data: `sendHomeWork-${chatId}`,
+        },
+      ],
+      [
+        {
+          text: 'Запрос переноса урока',
+          callback_data: `requestMoveLesson-${chatId}`,
+        },
+      ],
+    ],
+  }),
+});
+
+const gradeLesson = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [
+        {
+          text: '⭐',
+          callback_data: `gradeLesson-1`,
+        },
+      ],
+      [
+        {
+          text: '⭐⭐',
+          callback_data: `gradeLesson-2`,
+        },
+      ],
+      [
+        {
+          text: '⭐⭐⭐',
+          callback_data: `gradeLesson-3`,
+        },
+      ],
+      [
+        {
+          text: '⭐⭐⭐⭐',
+          callback_data: `gradeLesson-4`,
+        },
+      ],
+      [
+        {
+          text: '⭐⭐⭐⭐⭐',
+          callback_data: `gradeLesson-5`,
+        },
+      ],
+    ],
+  }),
+};
+
+/* const formOfHomeWork = (currentStudentChatId) => ({
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [
+        {
+          text: 'Картинка/фото',
+          callback_data: `pictureHW-${currentStudentChatId}`,
+        },
+      ],
+      [
+        {
+          text: 'Документ',
+          callback_data: `documentHW-${currentStudentChatId}`,
+        },
+      ],
+      [
+        {
+          text: 'Текст',
+          callback_data: `textHW-${currentStudentChatId}`,
+        },
+      ],
+    ],
+  }),
+}); */
 
 module.exports = {
-    studentOptions,
-    teacherOptions,
-    teachers: teachersOptions,
-    packsRussian,
-    packsNative,
-    supportOptions,
-    studentLeasonOptions
+  studentOptions,
+  teacherOptions,
+  teachers: teachersOptions,
+  packsRussian,
+  packsNative,
+  supportOptions,
+  teacherStudentOptions,
+  gradeLesson /* 
+  formOfHomeWork, */,
 };
-  
