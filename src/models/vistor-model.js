29777e695cda7e364cf350216457d8e93de8112c
moskalevNick <moskalevnikolay1@gmail.com>
2022-02-21 08:51:51 +0300

    fix models

--- src/models/vistor-model.js
+++ src/models/vistor-model.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 const VistorSchema = new Schema({
   first_name: { type: String, required: true },
   last_name: { type: String },
-  username: { type: String, required: true },
+  username: { type: String },
   is_bot: { type: Boolean, required: true },
   chatId: { type: Number, required: true },
   lastEntrance: { type: String, required: true },
